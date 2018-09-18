7ae2f5f69bec64942125e2ae85f8742197fdc233
Jon Cinque <jon.cinque@gmail.com>
2018-09-18 21:02:49 +0200

    Fixup project files for nuget

--- AcceptanceTest/runat.ps1
+++ AcceptanceTest/runat.ps1
@@ -2,7 +2,7 @@
 Param(
   [Parameter(Mandatory=$True,Position=1)]
    [string]$configuration,
-	
+
   [Parameter(Mandatory=$True,Position=2)]
    [string]$port,
 
@@ -10,10 +10,10 @@ Param(
    [string]$tests=$null,
 
   [Parameter(Position=4)]
-    [string] $conf= "cfg\at.cfg", 
+    [string] $conf= "cfg\at.cfg",
 
   [Parameter(Position=5)]
-    [string] $framework= "net461"
+    [string] $framework= "netcoreapp2.1"
 )
 
 $result = 0
@@ -25,7 +25,7 @@ if([string]::IsNullOrEmpty($tests))
 }
 
 
-if(($configuration -ne "release" -And $configuration -ne "debug") -Or ($framework -ne "net461" -And $framework -ne "netcoreapp2.0"))
+if(($configuration -ne "release" -And $configuration -ne "debug") -Or ($framework -ne "netcoreapp2.1"))
 {
     Usage
 }
@@ -50,8 +50,8 @@ function StartTests
     Invoke-Expression "ruby Runner.rb 127.0.0.1 $port $tests > TestResult.xml"
 
     if($LASTEXITCODE -ne 0){ $script:result = 1 }
-       
-    KillChildren $pid 
+
+    KillChildren $pid
 
     type TestResult.xml
     Quit
@@ -59,7 +59,7 @@ function StartTests
 
 function Usage
 {
-    echo "Usage: runat [release | debug] [port] [TESTFILE...] [net461|netcoreapp2.0]"
+    echo "Usage: runat [release | debug] [port] [TESTFILE...] [netcoreapp2.1]"
     $script:result = 1
     Quit
 }
--- acceptance_test.ps1
+++ acceptance_test.ps1
@@ -6,10 +6,10 @@ function TestSpec
     param(
      [Parameter(Mandatory=$True,Position=0)]
       [string]$configuration,
-	
+
      [Parameter(Mandatory=$True,Position=1)]
       [string]$framework,
-	
+
      [Parameter(Mandatory=$True,Position=2)]
      [string]$port,
 
@@ -39,23 +39,23 @@ function RunSuite
     param(
      [Parameter(Mandatory=$True,Position=0)]
       [string]$configuration,
-	
+
      [Parameter(Mandatory=$True,Position=1)]
       [string]$framework
-    )   
+    )
 
     # Fix 40
     TestSpec $configuration $framework "5001" "definitions/server/fix40/*.def" "cfg/at_40.cfg" "AcceptanceTests_40_$framework.xml"
 
     # Fix 41
     TestSpec $configuration $framework "5002" "definitions/server/fix41/*.def" "cfg/at_41.cfg" "AcceptanceTests_41_$framework.xml"
-    
+
     # Fix 42
     TestSpec $configuration $framework "5003" "definitions/server/fix42/*.def" "cfg/at_42.cfg" "AcceptanceTests_42_$framework.xml"
 
-    # Fix 43    
+    # Fix 43
     TestSpec $configuration $framework "5004" "definitions/server/fix43/*.def" "cfg/at_43.cfg" "AcceptanceTests_43_$framework.xml"
-    
+
     # Fix 44
     TestSpec $configuration $framework "5005" "definitions/server/fix44/*.def" "cfg/at_44.cfg" "AcceptanceTests_44_$framework.xml"
 
@@ -64,24 +64,22 @@ function RunSuite
 
     # Fix 50
     TestSpec $configuration $framework "5006" "definitions/server/fix50/*.def" "cfg/at_50.cfg" "AcceptanceTests_50_$framework.xml"
-    
+
     # Fix 50sp1
     TestSpec $configuration $framework "5007" "definitions/server/fix50sp1/*.def" "cfg/at_50_sp1.cfg" "AcceptanceTests_50_SP1_$framework.xml"
-    
+
     # Fix 50sp2
     TestSpec $configuration $framework "5008" "definitions/server/fix50sp2/*.def" "cfg/at_50_sp2.cfg" "AcceptanceTests_50_SP2_$framework.xml"
-	
+
     # Misc
     TestSpec $configuration $framework "5003" "definitions/server/misc/*.def" "cfg/at_42.misc.cfg" "AcceptanceTests_Misc_$framework.xml"
 }
 
 Push-Location -Path "AcceptanceTest" -StackName AcceptanceTest
-    
+
     Remove-Item AcceptanceTests_*.xml
 
-    RunSuite "release" "net461"
-
-    RunSuite "release" "netcoreapp2.0"
+    RunSuite "release" "netcoreapp2.1"
 
 Pop-Location -StackName AcceptanceTest
 
